de131c871e69e22942e541cb577abfdd6e25ed70
konetagyorgy <konetagyorgy@gmail.com>
2026-02-19 07:35:19 -0500

    Web deployment

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emot_detector():
+    text_to_analyse = request.args.get("textToAnalyze")
+    response = emotion_detector(text_to_analyse)
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+    return "For the given statement, the system response is: <br>'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {}, 'sadness': {}. <br>The dominant emotion is: {}.".format(anger, disgust, fear, joy, sadness, dominant_emotion)
+
+@app.route("/")
+def render_index_page():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5000)
